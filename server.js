ad8b910911114bb5d8e539880785e6d76bd0fb32
Makai-Maetani <102011493+Makai-Maetani@users.noreply.github.com>
2024-02-22 11:20:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,6 @@ app.get('/', (req,res) => {res.send("Hello Browser")});
 
 app.get('/logan', (req,res) => {res.send("Hello Logan")});
 app.get('/jesse', (req,res) => {res.send("Hello Jesse")});
+app.get('/makai', (req,res) => {res.send("Hello Makai")}); 
 
 app.listen(port, ()=>console.log("Listening"));
